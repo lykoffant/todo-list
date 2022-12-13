d2d2cf6690a7eba6785076e490ebde7feecc98dd
Anton Lykov <99159998+lykoffant@users.noreply.github.com>
2022-12-13 14:39:49 +0300

    build: add React Bootstrap front-end framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
 function App() {
-  return <h1>Todo List App</h1>;
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Todo List App</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
