87fa58703e168e332b391d6b7e59050b36be5d05
Anton Lykov <99159998+lykoffant@users.noreply.github.com>
2023-02-03 04:06:17 +0300

    feat: sync localStorage and root state

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,38 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import todosReducer from './todosSlice';
 
+function saveToLocalStorage(rootState: RootState) {
+  try {
+    const serializedRootState = JSON.stringify(rootState);
+
+    localStorage.setItem('rootState', serializedRootState);
+  } catch (e) {
+    console.warn(e);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serializedRootState = localStorage.getItem('rootState');
+
+    return serializedRootState === null
+      ? undefined
+      : JSON.parse(serializedRootState);
+  } catch (e) {
+    console.warn(e);
+
+    return undefined;
+  }
+}
+
 const store = configureStore({
   reducer: {
     todos: todosReducer,
   },
+  preloadedState: loadFromLocalStorage(),
 });
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store;
 
